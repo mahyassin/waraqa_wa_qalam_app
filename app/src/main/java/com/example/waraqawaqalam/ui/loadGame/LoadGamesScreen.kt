61e88e9d37e1b06f214576da71f62ceb7482e198
mahmoud yassin <modeyibrahimalter@gmail.com>
2024-11-18 12:50:43 +0200

    adding delete button
    adding the ui and the functionaliyt of a delete button in the loading screen

--- app/src/main/java/com/example/waraqawaqalam/ui/loadGame/LoadGamesScreen.kt
+++ app/src/main/java/com/example/waraqawaqalam/ui/loadGame/LoadGamesScreen.kt
@@ -6,17 +6,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,11 +50,12 @@ fun LoadGameScreen(
     gamesList: List<Kingdom>,
     loadingVM: LoadingVM = viewModel(),
     loadGame: (Int) -> Unit,
-    navigateUp: () -> Unit
+    navigateUp: () -> Unit,
+    deleteAll: () -> Unit,
     ) {
 
-
-
+    var displayedGames = loadingVM.getGames(gamesList)
+    var screenRecomposer by remember {  mutableStateOf(true) }
     Scaffold(
         topBar = {
             ScreenTopBar(
@@ -56,13 +65,46 @@ fun LoadGameScreen(
             )
         }
     ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            screenRecomposer
             Divider()
-            LazyColumn {
-                items(loadingVM.getGames(gamesList)) {
+            LazyColumn(Modifier.weight(12f)) {
+                items(displayedGames) {
                     GameCard(it) { loadGame (it[0].gameId)}
                 }
             }
+            DeleteButton(
+                Modifier.weight(2f),
+                deleteAll = { deleteAll()
+                    displayedGames = emptyList()
+                    screenRecomposer = !screenRecomposer
+                }
+            )
+
+        }
+    }
+}
+@Composable
+fun DeleteButton(
+    modifier: Modifier = Modifier,
+    deleteAll: () -> Unit,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ){
+        Divider(
+            thickness = 2.dp,
+        )
+        OutlinedButton(
+            onClick = { deleteAll()},
+            Modifier.padding(8.dp)
+        ) {
+            Text("Delete")
         }
     }
 }
@@ -107,6 +149,7 @@ fun GameCard(
                 )
             }
         )
+        Divider()
     }
 }
 
@@ -131,6 +174,8 @@ fun LoadGameScreenPreview() {
     LoadGameScreen(
         emptyList(),
         loadGame = { },
-        navigateUp = {}
+        navigateUp = {},
+
+        deleteAll = {  }
     )
 }
